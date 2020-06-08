3d4a71d736d1f41a671166bfb2797c8ffc89534f
SANCHIT KAUL <sanchitkaul99@github.com>
2020-06-08 16:23:49 +0530

    Added Country Picker and bar chart and final finishing touches

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
+import coronaImage from './images/image.png';
 
 class App extends React.Component{
 
     state = {
         data: {},
+        country:' ',
     }
 
     async componentDidMount() {
@@ -15,16 +17,24 @@ class App extends React.Component{
         this.setState({ data: fetchedData });
       }
 
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({ data: fetchedData , country: country });
+
+           
+    }  
 
     render() {
 
-        const { data } = this.state;
+        const { data , country } = this.state;
 
         return(
             <div className={styles.container}>
+                <img className={styles.image} src = {coronaImage} alt= "COVID-19"/>
                 <Cards data={data} />
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
             </div>
         )
     }
